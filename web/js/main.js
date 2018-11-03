aa3a94ff81d2a4ef7035b64a78873db99101cd8e
Attila Bagossy <bagossyattila@outlook.com>
2018-11-03 10:32:13 +0100

    Downloading the dataset

--- web/js/main.js
+++ web/js/main.js
@@ -1,10 +1,18 @@
-(function main() {
-    makePitch({
+(function mainIIFE() {
+    downloadDataset()
+        .then(dataset => makePitch(pitchStyle, dataset));
+
+    var pitchStyle = {
         selector: 'body',
         width: '1150px',
         height: '780px',
         lineColor: '#FFFFFF',
         lineWidth: 5,
         grassColor: "#A1C349"
-    });    
+    };
+
+    function downloadDataset() {
+        return fetch('data/shots.json')
+            .then(response => response.json())
+    }
 })();
