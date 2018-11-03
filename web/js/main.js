96872793e4121b4c9f82ebae99943b7479389315
Attila Bagossy <bagossyattila@outlook.com>
2018-11-03 10:22:17 +0100

    Drawing the pitch

---
+++ web/js/main.js
@@ -0,0 +1,10 @@
+(function main() {
+    makePitch({
+        selector: 'body',
+        width: '1150px',
+        height: '780px',
+        lineColor: '#FFFFFF',
+        lineWidth: 5,
+        grassColor: "#A1C349"
+    });    
+})();
